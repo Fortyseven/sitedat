b7d348750c0f735a3896c9df4a04f1f0b654f9d7
Fortyseven <fortyseven@network47.org>
2025-10-23 11:53:53 -0400

    feat: adds theme detection and style.css llisting; output cleanup

--- app/handlers/wordpress.py
+++ app/handlers/wordpress.py
@@ -4,8 +4,47 @@ console = Console()
 
 
 def wp_handler(args):
-    """WordPress handler"""
+    wp_theme_handler(args)
+    console.print("\n")
     wp_xmlrpc_handler(args, f"{args.url}/xmlrpc.php", None)
+
+
+def wp_theme_handler(args):
+    # this will look inside the index for a reference to the current theme `/wp-content/themes/theme-name`
+    theme = None
+    import requests
+    from bs4 import BeautifulSoup
+
+    try:
+        response = requests.get(args.url, timeout=10)
+        soup = BeautifulSoup(response.text, "html.parser")
+        for link in soup.find_all("link", href=True):
+            href = link["href"]
+            if "/wp-content/themes/" in href:
+                theme = href.split("/wp-content/themes/")[1].split("/")[0]
+                console.print(
+                    f"\n - [green bold]Found WordPress theme:[/green bold] {theme}"
+                )
+                stylescss = f"{args.url}/wp-content/themes/{theme}/style.css"
+                styles_response = requests.get(stylescss, timeout=10)
+                if styles_response.status_code == 200:
+                    console.print(
+                        f"   - [blue]Style.css found (size: {len(styles_response.content)} bytes):[/blue] {stylescss}"
+                    )
+                    # extract content between /* and */
+                    content = styles_response.text
+                    start = content.find("/*")
+                    end = content.find("*/", start)
+                    if start != -1 and end != -1:
+                        style_content = content[start + 2 : end].strip()
+                        for line in style_content.splitlines():
+                            console.print(f"      [green]{line.strip()}[/green]")
+                else:
+                    console.print(f"   - [red]Style.css not found:[/red] {stylescss}")
+                return
+        console.print("   - [red]No WordPress theme detected?[/red]")
+    except Exception as e:
+        console.print("   - [red]Error while trying to find WordPress theme[/red]", e)
 
 
 def wp_xmlrpc_handler(args, url, content):
@@ -16,8 +55,8 @@ def wp_xmlrpc_handler(args, url, content):
 
     try:
         server = xmlrpc.client.ServerProxy(url)
-        console.log(f" - {url} system.listMethods(): ")
-        print("\n - ", server.system.listMethods())
+        console.print(f" - {url} system.listMethods(): ")
+        console.print("\n - ", server.system.listMethods())
 
     except Exception as e:
-        console.log("Tried to query XML-RPC methods, but got an error: ", e)
+        console.print("   - [red]Tried to query XML-RPC methods but failed.[/red]")
